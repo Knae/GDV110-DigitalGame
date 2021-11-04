7e3f6db9983e9cb41a63aa6bbc627f3d36e01a00
Knae <general124@hotmail.com>
2021-11-05 12:06:42 +1300

    -Modified dialogue script to activate on entering trigger instead of on button press -Implemented prototype tilemap onto background

--- TroyPrototype/Assets/Scripts/Dialog/DialogueActivator.cs
+++ TroyPrototype/Assets/Scripts/Dialog/DialogueActivator.cs
@@ -5,23 +5,27 @@ public class DialogueActivator : MonoBehaviour, IInteractable
 {
 
     [SerializeField] private DialogueObject dialogueObject;
+    [SerializeField] public DialogueUI DialogueDisplay;
+
+    private bool bDialogueDone = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerDialog player))
+        if (other.CompareTag("Player") && !bDialogueDone/* && other.TryGetComponent(out PlayerDialog player)*/)
         {
-            player.Interactable = this;
+            DialogueDisplay.ShowDialogue(dialogueObject);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerDialog player))
+        if (other.CompareTag("Player")/* && other.TryGetComponent(out PlayerDialog player)*/)
         {
-            if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this)
-            {
-                player.Interactable = null;
-            }
+            //if (player.Interactable is DialogueActivator dialogueActivator && dialogueActivator == this)
+            //{
+            //    player.Interactable = null;
+            //}
+            bDialogueDone = true;
         }
     }
 
