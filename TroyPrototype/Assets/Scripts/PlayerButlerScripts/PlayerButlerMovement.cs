cb80c3f4e534e28fcc087170285669d82d60ac39
Knae <general124@hotmail.com>
2021-11-25 10:27:02 +1300

    Restoring files to last working state

--- TroyPrototype/Assets/Scripts/PlayerButlerScripts/PlayerButlerMovement.cs
+++ TroyPrototype/Assets/Scripts/PlayerButlerScripts/PlayerButlerMovement.cs
@@ -2,25 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class PlayerButlerMovement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
-    public GameObject gunObject;
+    public GameObject gunObject; 
+    public SpriteRenderer gunObjectSprite;
     public Animator playerTorsoAnimator;
     public Animator playerLegsAnimator;
     public Camera gameCam;
     public HealthBar healthbar;
-  
-
 
     [Header("Movement")]
     [SerializeField] private float m_fMoveSpeed = 2.0f;
 
     [Header("Variables")]
     [SerializeField] private float maxHealth = 100;
-    [SerializeField] public float currentHealth;
+    [SerializeField] private float currentHealth;
     [SerializeField] private float angle = 0.0f;
 
     [Header("Debug")]
@@ -28,21 +26,16 @@ public class PlayerButlerMovement : MonoBehaviour
     [SerializeField] private bool m_bGodMode = true;
     [SerializeField] private bool m_bHasHealthBar;
 
-    [SerializeField] private bool canRespawn = false;
-
     private Vector2 movementvector = new Vector3(0, 0, 0);
     private Vector2 mousePos = new Vector3(0, 0, 0);
     private SpriteRenderer m_playerLegsRenderer;
+    private SpriteRenderer m_playerTorsoRenderer;
 
-    //respawn stuff
-    private Vector3 respawnPoint;
- 
 
     void Start()
     {
         currentHealth = maxHealth;
-
-        if (healthbar!=null)
+        if(healthbar!=null)
         {
             healthbar.SetMaxHealth(maxHealth);
             m_bHasHealthBar = true;
@@ -52,8 +45,7 @@ public class PlayerButlerMovement : MonoBehaviour
             m_bHasHealthBar = false;
         }
         m_playerLegsRenderer =  transform.Find("PlayerLegs").GetComponent<SpriteRenderer>();
-
-        respawnPoint = transform.position;
+        m_playerTorsoRenderer = transform.Find("PlayerTorso").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -68,14 +60,6 @@ public class PlayerButlerMovement : MonoBehaviour
 
         movementvector.x = Input.GetAxis("Horizontal") * m_fMoveSpeed;
         movementvector.y = Input.GetAxis("Vertical") * m_fMoveSpeed;
-
-        Die();
-        Respawn();
-
-       
-
-
-
     }
 
     void FixedUpdate()
@@ -83,6 +67,15 @@ public class PlayerButlerMovement : MonoBehaviour
         //lookDir = mousePos - gunObject.transform.position;
         angle = FindAngleBetweenPoints(mousePos, transform.position);
         gunObject.transform.rotation = Quaternion.Euler( 0f, 0f, angle);
+        if (Mathf.Abs(angle) > 90)
+        {
+            gunObjectSprite.flipY = true;
+        }
+        else
+        {
+            gunObjectSprite.flipY = false;
+        }
+
 
         //process movement
         playerBody.velocity = movementvector;
@@ -95,12 +88,16 @@ public class PlayerButlerMovement : MonoBehaviour
         if (playerTorsoAnimator.GetFloat("Horizontal") > 0)
         {
             m_playerLegsRenderer.flipX = false;
+            m_playerTorsoRenderer.flipX = false;
             playerLegsAnimator.SetBool("Flipped", false);
+            //playerTorsoAnimator.SetBool("Flipped", false);
         }
         else if (playerTorsoAnimator.GetFloat("Horizontal") < 0)
         {
             m_playerLegsRenderer.flipX = true;
+            m_playerTorsoRenderer.flipX = true;
             playerLegsAnimator.SetBool("Flipped", true);
+            //playerTorsoAnimator.SetBool("Flipped", true);
         }
     }
 
@@ -108,24 +105,19 @@ public class PlayerButlerMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "ProjectileEnemy")
         {
-            float damage = collision.gameObject.GetComponent<BulletLifetime>().GetDamage();
+            float damage = 0;
+            if (collision.gameObject.GetComponent<BulletLifetime>()!=null)
+            {
+                damage = collision.gameObject.GetComponent<BulletLifetime>().GetDamage();
+            }
+            else if(collision.gameObject.GetComponent<ExplosiveProjectile>() != null)
+            {
+                damage = collision.gameObject.GetComponent<ExplosiveProjectile>().GetDamage();
+            }
             TakeDamage(damage);
             Destroy(collision.gameObject);
         }
-        else if (collision.gameObject.tag == "SetSpawn")
-        {
-            canRespawn = true;
-            Debug.Log("OAJHGO");
-            respawnPoint = transform.position;
-            Destroy(collision.gameObject);
-        }
     }
-
-    //private void OnTriggerEnter2D(Collision2D col)
-    //{
-        
-    //}
-
 
     private float FindAngleBetweenPoints(Vector3 _inPointA, Vector3 _inPointB)
     {
@@ -141,39 +133,8 @@ public class PlayerButlerMovement : MonoBehaviour
         }
     }
 
-    public void AddHealth(int _addHealth)
+    public void HealPlayer(float _input)
     {
-        if (!m_bGodMode)
-        {
-            currentHealth += _addHealth;
-        }
+        currentHealth += _input;
     }
-
-    //Respwan and Death
-
-    
-
-
-    void Respawn()
-    {
-    
-            if (currentHealth <= 0)
-            {
-                canRespawn = false;
-                Debug.Log("YOU DIED");
-                currentHealth = maxHealth;
-                transform.position = respawnPoint;
-                
-            }
-       
-    }
-
-    void Die()
-    {
-        if (currentHealth <= 0 && canRespawn == false)
-        {
-            SceneManager.LoadScene("DeathMenuTest");
-        }
-    }
-
 }
