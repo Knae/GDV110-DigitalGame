cb3f6f7ed95d130813cd726a5d2f68edb9e3e137
OMaloso <88115296+OMaloso@users.noreply.github.com>
2021-11-25 16:54:40 +1300

    Added elevator. Updated scene trigger to require E key press. Fixed some tables that could be walked through. Added some lab desks to indestructables.

--- TroyPrototype/Assets/Scripts/LoadScene.cs
+++ TroyPrototype/Assets/Scripts/LoadScene.cs
@@ -10,16 +10,39 @@ public class LoadScene : MonoBehaviour
     //public Object sceneToLoad;
     public string sceneName = "Level1Lobby";
 
+    bool bTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
+
             //Destroy(collision.gameObject);
             //SceneManager.LoadScene("LevelMiddle");
-            SceneManager.LoadScene(sceneName);
+            bTriggered = true;
 
             //SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+
+            //Destroy(collision.gameObject);
+            //SceneManager.LoadScene("LevelMiddle");
+            bTriggered = false;
+
+            //SceneManager.LoadScene(sceneToLoad.name);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && bTriggered == true)
+        {
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
